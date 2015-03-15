5248f61d5200f3b88b86229865d9f16ede7d3e6e
luddward <dv13lan@cs.umu.se>
2015-03-16 00:32:57 +0100

    Implemented Error handling

--- httpServer.go
+++ httpServer.go
@@ -58,8 +58,17 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 
 //Renders a given HTML template
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
+	t, err := template.ParseFiles(tmpl + ".html")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	err = t.Execute(w, p)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 //Save function
@@ -67,7 +76,12 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/save/"):]
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	p.save()
+	err := p.save()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
