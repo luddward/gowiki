57c6ec941ccf00d4b500f34ffc1d33680bb7e51a
luddward <dv13lan@cs.umu.se>
2015-03-16 00:21:02 +0100

    Added HTML templates and added Edit function

--- httpServer.go
+++ httpServer.go
@@ -1,16 +1,64 @@
 package main
 
+//Imports
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 )
 
-func handler(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(rw, "Hi there, i love %S!", req.URL.Path[1:])
+//Data struct for article
+type Page struct {
+	Title string
+	Body  []byte
 }
 
+/**
+Method: Save
+Takes a reciever, a pointer to Page
+*/
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+//load function
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Page{Title: title, Body: body}, nil
+}
+
+//view function
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	renderTemplate(w, "view", p)
+}
+
+//Edit function.
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
+//Main function
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	//http.HandleFunc("/save/"), saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
